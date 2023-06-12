231ba6eee9b3684fa4c908d12c77798731428f65
Ada Omoji <ada.omoji@gmail.com>
2023-06-12 22:36:00 +0200

    Fetch weather foreacast using OpenWeather API

--- script.js
+++ script.js
@@ -5,4 +5,15 @@ let backgroundImage = document.querySelector("body");
 
 submit.addEventListener('click', function(e) {
     e.preventDefault();
-})
+
+    const apiKey = '8bbae7804b09e5427b9cb9d69754333e'
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&units=metric&lat={lat}&lon={lon}&appid=${apiKey}`;
+    fetch (url)
+    .then (response =>response.json())
+    .then (data => {
+        (console.log(data))
+
+
+
+}) // end of weather fetch
+}) //end of event listener
