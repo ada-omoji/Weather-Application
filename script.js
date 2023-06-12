249890ef5b40ca51b22d851bc3f95b84cbad75b0
Ada Omoji <ada.omoji@gmail.com>
2023-06-12 22:23:12 +0200

    Create variables based on html classes

---
+++ script.js
@@ -0,0 +1,4 @@
+let input = document.querySelector('.cityInput');
+let submit = document.querySelector(".submitButton");
+let city = document.querySelector(".currentCity");
+let backgroundImage = document.querySelector("body");
