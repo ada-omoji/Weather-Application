f40b137175926ae720c655af582db83dce10fba6
Ada Omoji <ada.omoji@gmail.com>
2023-06-12 22:25:35 +0200

    Add event listener to submit button

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let input = document.querySelector('.cityInput');
 let submit = document.querySelector(".submitButton");
 let city = document.querySelector(".currentCity");
 let backgroundImage = document.querySelector("body");
+
+submit.addEventListener('click', function(e) {
+    e.preventDefault();
+})
