c7cbf604cf2ec5fdc457e2ca3f6e2a1755732fc9
Ada Omoji <ada.omoji@gmail.com>
2023-06-12 23:34:47 +0200

    Remove comments and restructure spacing for clarity

--- script.js
+++ script.js
@@ -4,20 +4,22 @@ let city = document.querySelector(".currentCity");
 let backgroundImage = document.querySelector("body");
 
 submit.addEventListener('click', function(e) {
-    e.preventDefault();
+e.preventDefault();
 
-    const apiKey = '8bbae7804b09e5427b9cb9d69754333e'
-    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&units=metric&lat={lat}&lon={lon}&appid=${apiKey}`;
-    fetch (url)
-    .then (response =>response.json())
-    .then (data => {
-        (console.log(data))
+const apiKey = '8bbae7804b09e5427b9cb9d69754333e'
 
     let display = document.querySelector(".weatherContainer");
     while (display.lastChild) {
         display.lastChild.remove();
     }
 
+    let url = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&units=metric&lat={lat}&lon={lon}&appid=${apiKey}`;
+    fetch (url)
+    .then (response =>response.json())
+    .then (data => {
+        (console.log(data))
+
+   
     for (let i = 0; i < data.list.length; i+= 8) { //iterate through the list array and retrieve data every 8th index (5 day forecast)     
         let date = new Date(data.list[i].dt * 1000).toLocaleString("en-BE", 
             {
@@ -32,30 +34,28 @@ submit.addEventListener('click', function(e) {
              let max = data.list[i].main.temp_max.toFixed(0)  + '°C';
              let humidity = data.list[i].main.humidity + ' %';
              let description = data.list[i].weather[0].description;
-            
-
             let icon = data.list[i].weather[0].icon;
             let iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
             let results = document.createElement('div')
-                    results.classList.add('weather')
-                    results.innerHTML = 
-                        `<p class = "date">${date}<p>
-                        <img src=${iconUrl}>
-                        <p>${description}</p>
-                        <div class = "minMax">
-                            <p>High: ${max}</p>
-                            <p>Low: ${min}</p>
-                        </div>
-                        <p>Humidity: ${humidity}</p>`
+            results.classList.add('weather')
+            results.innerHTML = 
+                `<p class = "date">${date}<p>
+                <img src=${iconUrl}>
+                <p>${description}</p>
+                <div class = "minMax">
+                    <p>High: ${max}</p>
+                    <p>Low: ${min}</p>
+                </div>
+                <p>Humidity: ${humidity}</p>`
     document.querySelector('.weatherContainer').appendChild(results)
 
     let cityValue = data.city.name + ', ' + data.city.country
     city.innerHTML = cityValue;
 
-}
-        }) 
-});
+}       // End of loop
+})     // End of fetch
+});   //End of event listener
 
 
 const submitButton = document.querySelector(".submitButton")
@@ -68,5 +68,6 @@ function background() {
           const image = data.results[0];
           backgroundImage.style.background = `url(${image.urls.regular})`;
           console.log('image')
-        }); //End of fetch 
-    }; //End of function
+
+        });    //End of fetch 
+    };         //End of function
